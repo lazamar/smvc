e06b07b044ce7d24ffa5bda37e39d70a58bd8373
Marcelo Lazaroni <lazaronijunior@gmail.com>
2024-03-31 19:04:19 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,235 @@
+const { init, h, text } = UI;
+
+const emit = v => _ => v;
+
+function update (s, msg) {
+  switch (true) {
+    case "Noop" in msg:
+      return s;
+    case "UpdateField" in msg:
+      const { UpdateField: value } = msg;
+      return Object.assign({}, s, {
+        field: value,
+      });
+    case "Add" in msg: {
+      return Object.assign({}, s,
+        { uid: s.uid + 1,
+          field: "",
+          entries: s.field.trim().length === 0
+            ? s.entries
+            : s.entries.concat([newEntry(s.field, s.uid)]),
+        },
+      );
+    }
+    case "EditingEntry" in msg: {
+      const { EditingEntry: { id, isEditing } } = msg;
+      requestAnimationFrame(() => {
+        const input = document.querySelector("#todo-" + id);
+        const end = input.value.length;
+        input.setSelectionRange(end, end);
+        input.focus();
+      });
+      return Object.assign({}, s, {
+        entries: s.entries.map(entry => {
+            if (entry.id == id) {
+              entry.editing = isEditing;
+            }
+            return entry;
+          })
+      });
+    }
+    case "UpdateEntry" in msg: {
+      const { UpdateEntry: { id, value } } = msg;
+      return Object.assign({}, s, {
+        entries: s.entries.map(entry => {
+            if (entry.id == id) {
+              entry.description = value;
+            }
+            return entry;
+          })
+      });
+    }
+    case "Delete" in msg: {
+      const { Delete : id } = msg;
+      return Object.assign({}, s, {
+        entries: s.entries.filter(e => e.id !== id)
+      });
+    }
+    case "Check" in msg: {
+      const { Check : { id, isCompleted } } = msg;
+      return Object.assign({}, s, {
+        entries: s.entries.map(entry => {
+          if (entry.id === id) {
+            entry.completed = isCompleted;
+          }
+          return entry
+        })
+      });
+    }
+  }
+  return s;
+}
+
+const newEntry = (description, id) =>
+  ({
+    description,
+    id,
+    completed: false,
+    editing: false,
+  });
+
+const view = (state) =>
+  [
+    h("div", { class: "todomvc-wrapper" }, [
+      h("section", { class: "todoapp" }, [
+        viewInput(state.field),
+        viewEntries(state.visibility, state.entries),
+        viewControls(state.visibility, state.entries)
+      ]),
+      infoFooter
+    ])
+  ]
+
+const noop = { Noop : true };
+
+function viewInput(value) {
+  return h("header", { class : "header" }, [
+    h("h1", {}, [text("todos")]),
+    h("input",
+      { class : "new-todo",
+        placeholder: "what needs to be done?",
+        autofocus: "true",
+        value: value,
+        name: "newTodo",
+        onInput: e => ({ UpdateField : e.target.value }),
+        onKeydown : onEnter({ Add : true })
+      }, [])
+  ]);
+}
+
+const onEnter = msg => event =>
+  (event.keyCode == 13)
+    ? msg
+    : noop
+
+function viewEntries(visibility, entries) {
+  const isVisible = todo =>
+        (visibility === "Completed")
+        ? todo.completed
+        : (visibility === "Active")
+        ? !todo.completed
+        : true;
+  const allCompleted = entries.every(e => e.completed);
+  const cssVisibility = entries.length === 0 ? "hidden" : "visible";
+  return h("section", { class : "main", style: `visibility: ${cssVisibility}` }, [
+    h("input", {
+      class: "toggle-all",
+      type: "checkbox",
+      checked: allCompleted,
+      onClick: emit({ CheckAll : !allCompleted })
+    },[]),
+    h("label", { for: "toggle-all" }, [
+      text("Mark all as complete")
+    ]),
+    h("ul", { class : "todo-list" },
+      entries.filter(e => isVisible(e)).map(viewEntry)
+    )
+  ]);
+}
+
+const classes = obj => Object.keys(obj).filter(key => obj[key]).join(" ");
+
+const viewEntry = (todo) =>
+  h("li", { class: classes({ completed: todo.completed, editing: todo.editing }) }, [
+    h("div", { class: "view" }, [
+      h("input", {
+        class: "toggle",
+        type: "checkbox",
+        checked: todo.completed,
+        onClick: emit({ Check: { id: todo.id, isCompleted: !todo.completed } })
+      }, []),
+      h("label", { onDblclick : emit({ EditingEntry : { id: todo.id, isEditing: true } }) }, [
+        text(todo.description)
+      ]),
+      h("button", { class: "destroy", onClick : emit({ Delete : todo.id  }) }, [])
+    ]),
+    h("input", {
+      class: "edit",
+      value: todo.description,
+      id: "todo-" + todo.id.toString(),
+      onInput: e => ({ UpdateEntry: { id: todo.id, value: e.target.value } }),
+      onBlur: emit({ EditingEntry: { id: todo.id, isEditing: false } }),
+      onKeydown: onEnter({ EditingEntry: { id: todo.id, isEditing: false } })
+      }, [])
+  ]);
+
+
+function viewControls(visibility, entries) {
+  const entriesCompleted = entries.filter(e => e.completed).length;
+  const entriesLeft = entries.length - entriesCompleted;
+  return h("footer", { class: "footer", hidden: entries.length === 0 }, [
+    viewControlsCount(entriesLeft),
+    viewControlsFilters(visibility),
+    viewControlsClear(entriesCompleted),
+  ]);
+}
+
+const viewControlsCount = (entriesLeft) =>
+  h("span", { class : "todo-count" }, [
+    h("strong",{},[
+      text(entriesLeft.toString())
+    ]),
+    text(entriesLeft === 1 ? " item left" : " items left")
+  ]);
+
+const viewControlsFilters = (visibility) =>
+  h("ul", { class: "filters" }, [
+    visibilitySwap("#/", "All", visibility),
+    text(" "),
+    visibilitySwap("#/active", "Active", visibility),
+    text(" "),
+    visibilitySwap("#/completed", "Completed", visibility),
+  ]);
+
+
+const visibilitySwap = (url, visibility, actualVisibility) =>
+  h("li", { onClick : emit({ ChangeVisibility : visibility }) }, [
+    h("a", { href: url, class: classes({ selected: visibility === actualVisibility }) }, [
+      text(visibility)
+    ])
+  ])
+
+const viewControlsClear = (entriesCompleted) =>
+  h("button", {
+    class: "clear-completed",
+    hidden: entriesCompleted === 0,
+    onClick: emit({ DeleteComplete: true }),
+  },[
+    text("Clear completed (" + entriesCompleted.toString() + ")")
+  ]);
+
+
+const infoFooter =
+  h("footer", { class: "info" }, [
+    h("p", {}, [ text("Double-click to edit a todo") ]),
+    h("p", {}, [
+      text("Written by "),
+      h("a", { href: "https://github.com/lazamar" }, [ text("Marcelo Lazaroni") ])
+    ]),
+    h("p", {}, [
+      text("Port of "),
+      h("a", { href: "http://todomvc.com" }, [ text("TodoMVC") ])
+    ]),
+  ]);
+
+const root = document.querySelector("#todomvc");
+const initialState = {
+  entries: [],
+  visibility: "All",
+  field: "",
+  uid: 0,
+};
+init(root, initialState, update, view);
+
+
+
---
+++ ui.js
@@ -0,0 +1,245 @@
+// Spec
+//    = { tag : string
+//      , attributes : { attribute: string }
+//      , listeners : { event: listener } -- support a single listener
+//      , children : [Spec]
+//      }
+//    | { textContent : string }
+//
+// ElementDiff
+//    = Replace Element
+//    | Remove
+//    | Create Element
+//    | Modify ContentsDiff
+//    | Noop
+//
+// ContentsDiff
+//    = { removeAttr :: [String]
+//      , setAttr :: { attribute: value }
+//      , removeListeners :: { event: f }
+//      , addListeners :: { event: f }
+//      , children : [ElementDiff]
+//      }
+//
+const UI = (function () {
+
+let props = ["value", "hidden"];
+function setAttribute(attr, value, el) {
+  el.setAttribute(attr, value);
+  if (props.includes(attr)) {
+    el[attr] = value;
+  }
+}
+
+function eventName(str) {
+  if (str.indexOf("on") == 0) {
+    return str.slice(2).toLowerCase();
+  }
+  return null;
+}
+
+// diff two specs
+function diffOne(l, r) {
+  let isText = l.textContent !== undefined;
+  if (isText) {
+    return l.textContent !== r.textContent
+      ? { replace: r }
+      : { noop: true };
+  }
+
+  if (l.tag !== r.tag) {
+    return { replace: r };
+  }
+
+  const removeAttr = [];
+  const setAttr = {};
+  const removeListeners = {};
+  const addListeners = {};
+  for (const attr in l.attributes) {
+    if (r.attributes[attr] === undefined) {
+      let event = eventName(attr);
+      if (event !== null) {
+        removeListeners[event] = l.attributes[attr];
+      } else {
+        removeAttr.push(attr);
+      }
+    }
+  }
+
+  for (const attr in r.attributes) {
+    if (r.attributes[attr] !== l.attributes[attr]) {
+      let event = eventName(attr);
+      if (event === null) {
+        setAttr[attr] = r.attributes[attr];
+      } else {
+        removeListeners[event] = l.attributes[attr];
+        addListeners[event] = r.attributes[attr];
+      }
+    }
+  }
+
+  const children = diffList(l.children, r.children);
+  const noChildrenChange = children.every(e => e.noop);
+  const noAttributeChange =
+        (removeAttr.length === 0) &&
+        (Array.from(Object.keys(setAttr)).length == 0)
+
+  if (noChildrenChange && noAttributeChange) {
+    return { noop : true }
+  }
+
+  return { modify: { removeAttr, setAttr, removeListeners, addListeners, children } };
+}
+
+function diffList(ls, rs) {
+  let len = Math.max(ls.length, rs.length);
+  let diffs = [];
+  for (let i = 0; i < len; i++) {
+    diffs.push(
+      ls[i] === undefined
+      ? { create: rs[i] }
+      : rs[i] == undefined
+      ? { remove: true }
+      : diffOne(ls[i], rs[i])
+    );
+  }
+  return diffs;
+}
+
+function create(enqueue, spec) {
+  if (spec.textContent !== undefined) {
+    let el = document.createTextNode(spec.textContent);
+    return el;
+  }
+
+  let el = document.createElement(spec.tag);
+
+  for (const attr in spec.attributes) {
+    let event = eventName(attr);
+    let value = spec.attributes[attr];
+
+    if (event && typeof value !== "function") {
+      throw Error(`Event listener for ${attr} is not a function`);
+    }
+
+    event
+      ? el.addEventListener(event, e => enqueue(value(e)))
+      : setAttribute(attr, value, el);
+  }
+
+  for (let i in spec.children) {
+    const childSpec = spec.children[i];
+    const child = create(enqueue, childSpec);
+    el.appendChild(child);
+  }
+
+  return el;
+}
+
+function modify(el, enqueue, diff) {
+  for (const attr in diff.removeAttr) {
+    el.removeAttribute(attr);
+  }
+  for (const attr in diff.setAttr) {
+    setAttribute(attr, diff.setAttr[attr], el);
+  }
+  for (const event in diff.removeListeners) {
+    el.removeEventListener(event, diff.removeListeners[event]);
+  }
+  for (const event in diff.addListeners) {
+    let handle = diff.addListeners[event];
+
+    if (typeof handle !== "function") {
+      throw Error(`Event listener for ${attr} is not a function`);
+    }
+
+    el.addEventListener(event, e => enqueue(handle(e)));
+  }
+  if (diff.children.length < el.childNodes.length) {
+    throw new Error("unmatched children lengths");
+  }
+
+  apply(el, enqueue, diff.children);
+}
+
+function apply(el, enqueue, childrenDiff) {
+  for (let i = 0, k = 0; i < childrenDiff.length; i++, k++) {
+    let diff = childrenDiff[i];
+    if (diff.remove) {
+      el.childNodes[k].remove();
+      k--;
+    } else if (diff.modify !== undefined) {
+      modify(el.childNodes[k], enqueue, diff.modify);
+    } else if (diff.create !== undefined) {
+      if (k < el.childNodes.length) {
+        throw new Error("Adding in the middle of children: " + k + " " + el.childNodes.length);
+      }
+      let child = create(enqueue, diff.create);
+      el.appendChild(child);
+    } else if (diff.replace !== undefined) {
+      let child = create(enqueue, diff.replace);
+      el.childNodes[k].replaceWith(child);
+    } else if (diff.noop) {
+    } else {
+      throw new Error("Unexpected diff option: " + Object.keys(diff));
+    }
+  }
+}
+
+// Create an HTML element
+function h(tag, attributes, children) {
+  if (children.includes(undefined)) {
+    throw new Error("Undefined children in: ", JSON.stringify(children));
+  }
+  return { tag, attributes, children };
+}
+
+// Create a text element
+function text(textContent) {
+  return { textContent }
+}
+
+// Start managing the contents of an HTML node.
+function init(root, initialState, update, view) {
+  let state = initialState; // client application state
+  let spec = []; // elements spec
+  let queue = []; // msg queue
+
+  function enqueue(msg) {
+    queue.push(msg);
+  }
+
+  // draws the current state
+  function draw() {
+    let newSpec = view(state);
+    apply(root, enqueue, diffList(spec, newSpec));
+    spec = newSpec;
+  }
+
+  function updateState() {
+    if (queue.length > 0) {
+      let msgs = queue;
+      queue = [];
+
+      msgs.forEach(msg => {
+        try {
+          state = update(state, msg, enqueue);
+        } catch (e) {
+          console.error(e);
+        }
+      });
+
+      draw();
+    }
+
+    window.requestAnimationFrame(updateState);
+  }
+
+  draw();
+  updateState();
+
+  return { enqueue };
+}
+
+return { init, h, text };
+})();
